b85b9df62047701572e3a40a0cf2e3f6b2460f4b
dx-mon <dx-mon@users.sourceforge.net>
2018-02-05 19:05:42 +0000

    Added the pixel shader implementation

---
+++ shade.cxx
@@ -0,0 +1,116 @@
+#include <array>
+#include <math.h>
+#include "mandelbrot.hxx"
+#include "shade.hxx"
+
+using jsonAtom_t = rSON::JSONAtom;
+using jsonArray_t = rSON::JSONArray;
+using jsonFloat_t = rSON::JSONFloat;
+using rSON::parseJSON;
+using jsonParserError_t = rSON::JSONParserError;
+
+using jsonAtomPtr_t = std::unique_ptr<jsonAtom_t>;
+
+std::unique_ptr<rgb8_t []> image{nullptr};
+std::array<floatRGB_t, 16> colours
+{{
+	rgb8_t{0x07, 0x00, 0x5D},
+	rgb8_t{0x11, 0x19, 0x87},
+	rgb8_t{0x1E, 0x4A, 0xAC},
+	rgb8_t{0x43, 0x76, 0xCD},
+	rgb8_t{0x86, 0xAF, 0xE1},
+	rgb8_t{0xD0, 0xE8, 0xF7},
+	rgb8_t{0xED, 0xE7, 0xBE},
+	rgb8_t{0xF5, 0xC9, 0x5A},
+	rgb8_t{0xFD, 0xA8, 0x01},
+	rgb8_t{0xC8, 0x81, 0x01},
+	rgb8_t{0x94, 0x54, 0x00},
+	rgb8_t{0x64, 0x31, 0x01},
+	rgb8_t{0x42, 0x12, 0x06},
+	rgb8_t{0x0E, 0x03, 0x0E},
+	rgb8_t{0x05, 0x00, 0x26},
+	rgb8_t{0x05, 0x00, 0x47}
+}};
+
+std::unique_ptr<std::atomic<uint32_t> []> imageStatus{nullptr};
+std::mutex imageMutex;
+std::condition_variable imageSync;
+
+inline floatRGB_t linearEase(const floatRGB_t &a, const floatRGB_t &b, const double amount) noexcept
+	{ return a + (b * amount); }
+
+rgb8_t colourFor(const double x) noexcept
+{
+	const auto normX = fmod(x, colours.size());
+	const uint8_t i = uint8_t(normX);
+	const auto frac = normX - floor(normX);
+
+	const auto colour1 = colours[i];
+	const auto colour2 = colours[(i + 1) % colours.size()] - colour1;
+	return linearEase(colour1, colour2, frac).toRGB8();
+}
+
+inline rgb8_t shade(const double i) noexcept
+{
+	if (i >= maxIterations)
+		return {};
+	return colourFor(i / 6.0);
+}
+
+inline size_t xy(const area_t point, const area_t &size) noexcept
+{
+	size_t x = point.width();
+	size_t y = point.height() * size.width();
+	return y + x;
+}
+
+void shadeChunk(const area_t size, const area_t subchunk, const uint32_t subdiv,
+	rSON::stream_t &stream, const uint32_t affinityOffset) noexcept
+{
+	area_t offset;
+	if (!image)
+		return;
+	threadAffinity(affinityOffset);
+	stream.read(offset);
+	offset *= subchunk;
+
+	printf("Shader launched for %u, %u\n", offset.width(), offset.height());
+	for (uint32_t y{0}; y < subchunk.height(); ++y)
+	{
+		for (uint32_t x{0}; x < subchunk.width(); ++x)
+		{
+			double point;
+			/*jsonAtomPtr_t pointsAtom;
+			try
+				{ pointsAtom.reset(parseJSON(stream)); }
+			catch (const jsonParserError_t &e)
+				{ fprintf(stderr, "%s\n", e.error()); abort(); }
+			if (!pointsAtom->typeIs(rSON::JSON_TYPE_ARRAY))
+				continue;
+			jsonArray_t &points = *pointsAtom;
+			if (points.count() != subdiv)
+				continue;*/
+
+			rgb16_t colour;
+			for (size_t i{0}; i < subdiv; ++i)
+			{
+				read(stream, point);
+				if (i == 0)
+					colour = shade(point/*s[i]*/);
+				else
+				{
+					colour += shade(point/*s[i]*/);
+					colour /= 2;
+				}
+			}
+			image[xy(area_t{x, y} + offset, size)] = colour;
+		}
+		++imageStatus[y + offset.height()];
+		if (imageStatus[y + offset.height()] == xTiles)
+		{
+			std::unique_lock<std::mutex> lock(imageMutex);
+			imageSync.notify_all();
+		}
+	}
+	puts("Shader done");
+}
---
+++ shade.hxx
@@ -0,0 +1,117 @@
+#ifndef SHADE__HXX
+#define SHADE__HXX
+
+#include <stdint.h>
+#include <type_traits>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include "mandelbrot.hxx"
+
+struct floatRGB_t;
+
+template<typename T> struct rgb_t final
+{
+protected:
+	T _r, _g, _b;
+	constexpr static T max = std::numeric_limits<T>::max();
+
+public:
+	using type = T;
+	constexpr rgb_t() noexcept : _r(0), _g(0), _b(0) { }
+	constexpr rgb_t(const T r, const T g, const T b) noexcept : _r(r), _g(g), _b(b) { }
+	template<typename U, typename = typename std::enable_if<!std::is_same<T, U>::value>::type>
+		constexpr rgb_t(const rgb_t<U> &pixel) noexcept : _r(pixel.r()), _g(pixel.g()), _b(pixel.b()) { }
+
+	rgb_t<T> operator +(const rgb_t<T> &pixel) const noexcept
+		{ return {T(_r + pixel._r), T(_g + pixel._g), T(_b + pixel._b)}; }
+	rgb_t<T> operator -(const rgb_t<T> &pixel) const noexcept
+		{ return {T(_r - pixel._r), T(_g - pixel._g), T(_b - pixel._b)}; }
+
+	template<typename U> void operator +=(const rgb_t<U> &pixel) noexcept
+	{
+		_r += pixel.r();
+		_g += pixel.g();
+		_b += pixel.b();
+	}
+
+	rgb_t<T> operator *(const double scalar) const noexcept
+		{ return {T(_r * scalar), T(_g * scalar), T(_b * scalar)}; }
+
+	void operator /=(const uint32_t scalar) noexcept
+	{
+		_r /= scalar;
+		_g /= scalar;
+		_b /= scalar;
+	}
+
+	constexpr T r() const noexcept { return _r; }
+	constexpr T g() const noexcept { return _g; }
+	constexpr T b() const noexcept { return _b; }
+
+	constexpr double floatR() const noexcept { return _r * 2.2 / max; }
+	constexpr double floatG() const noexcept { return _g * 2.2 / max; }
+	constexpr double floatB() const noexcept { return _b * 2.2 / max; }
+};
+
+using rgb8_t = rgb_t<uint8_t>;
+using rgb16_t = rgb_t<uint16_t>;
+
+struct floatRGB_t final
+{
+protected:
+	double _r, _g, _b;
+
+	constexpr static uint8_t clamp(const double value) noexcept
+		{ return std::max<double>(0, std::min<double>(255, value)); }
+
+	constexpr static uint8_t scaleClamp(const double value) noexcept
+		{ return clamp(value * 255); }
+
+public:
+	constexpr floatRGB_t() noexcept : _r(0), _g(0), _b(0) { }
+	constexpr floatRGB_t(const double r, const double g, const double b) noexcept : _r(r), _g(g), _b(b) { }
+	constexpr floatRGB_t(const rgb8_t &pixel) noexcept : _r(pixel.floatR()), _g(pixel.floatG()), _b(pixel.floatB()) { }
+
+	floatRGB_t operator +(const floatRGB_t &pixel) const noexcept
+		{ return {_r + pixel._r, _g + pixel._g, _b + pixel._b}; }
+	floatRGB_t operator -(const floatRGB_t &pixel) const noexcept
+		{ return {_r - pixel._r, _g - pixel._g, _b - pixel._b}; }
+
+	floatRGB_t operator *(const double scalar) const noexcept
+		{ return {_r * scalar, _g * scalar, _b * scalar}; }
+	floatRGB_t operator /(const double scalar) const noexcept
+		{ return {_r / scalar, _g / scalar, _b / scalar}; }
+
+	double r() const noexcept { return _r; }
+	double g() const noexcept { return _g; }
+	double b() const noexcept { return _b; }
+
+	rgb8_t toRGB8() const noexcept
+	{
+		const auto colour = *this / 2.2;
+		return {scaleClamp(colour.r()), scaleClamp(colour.g()), scaleClamp(colour.b())};
+	}
+};
+
+/*struct rgba8_t final : public rgb8_t
+{
+private:
+	uint8_t _a;
+
+public:
+	constexpr rgba8_t() noexcept : rgb8_t(), _a(0) { }
+	constexpr rgba8_t(const rgb8_t &pixel) : rgb8_t(pixel), _a(255) { }
+};*/
+
+extern std::unique_ptr<rgb8_t []> image;
+extern std::unique_ptr<std::atomic<uint32_t> []> imageStatus;
+extern std::mutex imageMutex;
+extern std::condition_variable imageSync;
+extern uint32_t xTiles;
+
+void shadeChunk(const area_t size, const area_t subchunk, const uint32_t subdiv,
+	rSON::stream_t &stream, const uint32_t affinityOffset) noexcept;
+
+#endif /*SHADE__HXX*/
