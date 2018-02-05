e8919bbd8ba7efa3e2f431d5e2c75a7a628195d9
dx-mon <dx-mon@users.sourceforge.net>
2018-02-05 19:06:00 +0000

    Added the master program header and the compute implmentation

---
+++ compute.cxx
@@ -0,0 +1,116 @@
+#include <stdint.h>
+#include <math.h>
+#include <fenv.h>
+#include <new>
+#include <thread>
+#include "mandelbrot.hxx"
+#include "ringBuffer.hxx"
+#include "memory.hxx"
+
+using jsonAtom_t = rSON::JSONAtom;
+using jsonArray_t = rSON::JSONArray;
+using jsonFloat_t = rSON::JSONFloat;
+using rSON::writeJSON;
+
+template<typename T> jsonAtom_t *nodeAlloc(T node)
+	{ return new (std::nothrow) T(node); }
+
+constexpr double power(double base, uint32_t exp) noexcept
+	{ return exp == 0 ? 1 : base * power(base, exp - 1); }
+
+constexpr static const double bailout = power(power(2, 8), 2);
+static const double log_2 = log(2);
+
+double computePoint(const point2_t p0) noexcept
+{
+	point2_t pp, p{};
+	uint16_t iteration{0};
+
+	for (; iteration < maxIterations; ++iteration)
+	{
+		pp = p * p;
+		if (pp.sum() > bailout)
+			break;
+		p.y(2 * p.mul() + p0.y());
+		p.x(pp.diff() + p0.x());
+	}
+
+	if (iteration < maxIterations && iteration)
+	{
+		pp = p * p;
+		const double zn = log(pp.sum()) / 2;
+		const double nu = log(zn / log_2) / log_2;
+		feclearexcept(FE_ALL_EXCEPT);
+		return iteration + 1 - nu;
+	}
+
+	return iteration;
+}
+
+void computeSubchunk(const area_t &size, const area_t &offset, const point2_t &scale,
+	const point2_t &origin, ringBuffer_t<double> &buffer) noexcept
+{
+	puts("Subpixel worker launched");
+	const uint32_t maxY = height - 1;
+	for (uint32_t y{0}; y < size.height(); ++y)
+	{
+		for (uint32_t x{0}; x < size.width(); ++x)
+		{
+			area_t pixel = offset + area_t{x, y};
+			pixel.height(maxY - pixel.height());
+			const double iteration = computePoint((pixel / scale) + origin);
+			buffer.write(iteration);
+		}
+	}
+	puts("Subpixel worker done");
+}
+
+void computeChunk(const area_t size, const area_t offset, const point2_t scale,
+	const point2_t center, const uint32_t subdiv, rSON::stream_t &stream) noexcept try
+{
+	const point2_t origin = -((area_t{width, height} / scale) / 2) + center;
+	const point2_t subpixelOrigin = -(point2_t{double(subdiv / 2), double(subdiv / 2)} / subdiv) / scale;
+	const point2_t subpixelOffset = (point2_t{1, 1} / subdiv) / scale;
+	const uint32_t totalSubdivs = subdiv * subdiv;
+	auto subpixels = makeUnique<ringBuffer_t<double> []>(totalSubdivs);
+	auto subchunkThreads = makeUnique<std::thread []>(totalSubdivs);
+	if (!subpixels || !subchunkThreads)
+		abort();
+
+	printf("Launching %u subpixel workers\n", totalSubdivs);
+	for (uint32_t y{0}; y < subdiv; ++y)
+	{
+		for (uint32_t x{0}; x < subdiv; ++x)
+		{
+			const point2_t subchunkOffset = (subpixelOffset * area_t{x, y}) + subpixelOrigin;
+			const uint32_t index = x + (y * subdiv);
+			subchunkThreads[index] = std::thread([&](const point2_t origin, const uint32_t index) noexcept
+				{
+					threadAffinity(index + 1);
+					computeSubchunk(size, offset, scale, origin, subpixels[index]);
+				}, origin + subchunkOffset, index
+			);
+		}
+	}
+
+	double iteration{0.0};
+	for (uint32_t y{0}; y < size.height(); ++y)
+	{
+		for (uint32_t x{0}; x < size.width(); ++x)
+		{
+			//jsonArray_t points;
+			for (uint32_t i{0}; i < totalSubdivs; ++i)
+			{
+				subpixels[i].read(iteration);
+				//points.add(nodeAlloc<jsonFloat_t>(iteration));
+				write(stream, iteration);
+			}
+			//writeJSON(&points, stream);
+		}
+	}
+
+	puts("Reaping subpixel workers");
+	for (uint32_t i{0}; i < totalSubdivs; ++i)
+		subchunkThreads[i].join();
+}
+catch (const std::bad_alloc &) { abort(); }
---
+++ mandelbrot.hxx
@@ -0,0 +1,110 @@
+#ifndef MANDELBROT__HXX
+#define MANDELBROT__HXX
+
+#include <stdint.h>
+#include <utility>
+#include <rSON.h>
+
+struct area_t final
+{
+private:
+	uint32_t _width, _height;
+
+public:
+	constexpr area_t() noexcept : _width(0), _height(0) { }
+	constexpr area_t(const uint32_t x, const uint32_t y) noexcept : _width(x), _height(y) { }
+
+	uint32_t width() const noexcept { return _width; }
+	void width(const uint32_t width) noexcept { _width = width; }
+	uint32_t height() const noexcept { return _height; }
+	void height(const uint32_t height) noexcept { _height = height; }
+
+	area_t operator +(const area_t point) const noexcept
+		{ return {_width + point._width, _height + point._height}; }
+	area_t operator -(const area_t point) const noexcept
+		{ return {_width - point._width, _height - point._height}; }
+	area_t operator *(const area_t point) const noexcept
+		{ return {_width * point._width, _height * point._height}; }
+	area_t operator /(const area_t point) const noexcept
+		{ return {_width / point._width, _height / point._height}; }
+
+	void operator *=(const area_t point) noexcept
+		{ _width *= point._width; _height *= point._height; }
+
+	void swap(area_t &point) noexcept
+	{
+		std::swap(_width, point._width);
+		std::swap(_height, point._height);
+	}
+};
+
+struct point2_t final
+{
+private:
+	double _x, _y;
+
+public:
+	constexpr point2_t() noexcept : _x(0), _y(0) { }
+	constexpr point2_t(const double x, const double y) noexcept : _x(x), _y(y) { }
+
+	double x() const noexcept { return _x; }
+	void x(const double x) noexcept { _x = x; }
+	double y() const noexcept { return _y; }
+	void y(const double y) noexcept { _y = y; }
+
+	point2_t operator +(const point2_t point) const noexcept
+		{ return {_x + point._x, _y + point._y}; }
+	point2_t operator -(const point2_t point) const noexcept
+		{ return {_x - point._x, _y - point._y}; }
+	point2_t operator *(const point2_t point) const noexcept
+		{ return {_x * point._x, _y * point._y}; }
+	point2_t operator /(const point2_t point) const noexcept
+		{ return {_x / point._x, _y / point._y}; }
+	point2_t operator -() const noexcept
+		{ return {-_x, -_y}; }
+
+	point2_t operator *(const area_t point) const noexcept
+		{ return {_x * point.width(), _y * point.height()}; }
+
+	point2_t operator /(const uint32_t scalar) const noexcept
+		{ return {_x / scalar, _y / scalar}; }
+
+	double mul() const noexcept { return _x * _y; }
+	double sum() const noexcept { return _x + _y; }
+	double diff() const noexcept { return _x - _y; }
+
+	void swap(point2_t &point) noexcept
+	{
+		std::swap(_x, point._x);
+		std::swap(_y, point._y);
+	}
+};
+
+inline point2_t operator /(const area_t a, const point2_t b) noexcept
+	{ return {a.width() / b.x(), a.height() / b.y()}; }
+
+constexpr static const int16_t maxIterations = 1000;
+extern uint32_t width, height;
+extern uint32_t xTiles, yTiles;
+extern std::vector<uint32_t> availableProcessors;
+
+void computeChunk(const area_t size, const area_t offset, const point2_t scale,
+	const point2_t center, const uint32_t subdiv, rSON::stream_t &stream) noexcept;
+
+inline void threadAffinity(const uint32_t affinityOffset) noexcept
+{
+	cpu_set_t affinity = {};
+	CPU_ZERO(&affinity);
+	CPU_SET(affinityOffset % availableProcessors.size(), &affinity);
+	pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &affinity);
+}
+
+template<typename T> bool read(rSON::stream_t &stream, T &value)
+{
+	size_t len;
+	return stream.read(&value, sizeof(T), len);
+}
+template<typename T> bool write(rSON::stream_t &stream, const T &value)
+	{ return stream.write(&value, sizeof(T)); }
+
+#endif /*MANDELBROT__HXX*/
